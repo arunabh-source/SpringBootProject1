4b2b8289e14d16ece40470a98e5ca73713340d9d
John Wolfe <jwolfe890@gmail.com>
2018-12-16 23:52:11 -0600

    compile group

--- src/main/java/sbootproject/security/AuthenticationFilter.java
+++ src/main/java/sbootproject/security/AuthenticationFilter.java
@@ -9,7 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import io.jsonwebtoken.*;
+import io.jsonwebtoken.Jwts;
 
 
 import org.springframework.security.authentication.AuthenticationManager;
