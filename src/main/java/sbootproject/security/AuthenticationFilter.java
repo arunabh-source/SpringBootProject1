62bcf8eb0189ba3342f77c22100d5d2c0ac26a37
John Wolfe <jwolfe890@gmail.com>
2018-12-17 01:02:45 -0600

    gradle build

--- src/main/java/sbootproject/security/AuthenticationFilter.java
+++ src/main/java/sbootproject/security/AuthenticationFilter.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
-import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
