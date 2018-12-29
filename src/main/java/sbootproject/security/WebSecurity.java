5b9d6482e0556a6aa1178ff72e3199f1c511a73a
John Wolfe <jwolfe890@gmail.com>
2018-12-29 17:08:17 -0600

    configured global cors

--- src/main/java/sbootproject/security/WebSecurity.java
+++ src/main/java/sbootproject/security/WebSecurity.java
@@ -1,11 +1,18 @@
 package sbootproject.security;
 
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import sbootproject.service.intrf.UserService;
 
@@ -22,14 +29,19 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-					.authorizeRequests()
+		 http
+	        .cors().and()
+	        .csrf().disable().authorizeRequests()
 					.antMatchers(HttpMethod.POST, SecurityConstants.SIGN_UP_URL)
 					.permitAll()
 					.anyRequest()
 					.authenticated()					
 					.and()
-					.addFilter(new AuthenticationFilter(authenticationManager()));
+					.addFilter(getAuthenticationFilter())
+					.addFilter(new AuthorizationFilter(authenticationManager()))
+					.sessionManagement()
+					.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+					
 
 //TURNING SPRING SECURITY OFF
 //		.antMatchers("/users")					
@@ -41,6 +53,28 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
 	}
+	
+    protected AuthenticationFilter getAuthenticationFilter() throws Exception {
+	    final AuthenticationFilter filter = new AuthenticationFilter(authenticationManager());
+	    filter.setFilterProcessesUrl("/users/login");
+	    return filter;
+	}
+    
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource()
+    {
+    	final CorsConfiguration configuration = new CorsConfiguration();
+    	   
+    	configuration.setAllowedOrigins(Arrays.asList("*"));
+    	configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE","OPTIONS"));
+    	configuration.setAllowCredentials(true);
+    	configuration.setAllowedHeaders(Arrays.asList("*"));
+    	
+    	final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    	source.registerCorsConfiguration("/**", configuration);
+    	
+    	return source;
+    }
 	
 	
 }
