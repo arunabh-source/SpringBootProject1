e08356d0ecf5d1fc83a6b4b0c9bf91d45d5add3c
John Wolfe <jwolfe890@gmail.com>
2018-12-29 19:33:29 -0600

    added authorization access header for jwt

--- src/main/java/sbootproject/security/WebSecurity.java
+++ src/main/java/sbootproject/security/WebSecurity.java
@@ -70,11 +70,19 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     	configuration.setAllowCredentials(true);
     	configuration.setAllowedHeaders(Arrays.asList("*"));
     	
+        configuration.setExposedHeaders(Arrays.asList("Access-Control-Allow-Headers", "Authorization, x-xsrf-token, Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, " +
+                "Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"));
+
+    	
     	final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     	source.registerCorsConfiguration("/**", configuration);
     	
     	return source;
     }
 	
+    
+    
+    
+    
 	
 }
